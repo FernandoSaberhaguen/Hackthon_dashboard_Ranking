30bec0159567c37e0770993fa8f307b097eb8051
Fernando-Aguilar1 <fernando.aguilar@baubap.com>
2023-08-23 17:24:53 -0600

    Change requirements.txt

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 # Función para cargar los datos
 def load_data(file_name: str):
@@ -13,35 +13,58 @@ st.title('Ranking Team')
 
 # Función para graficar los datos
 def plot_data(data, title):
-    fig, ax = plt.subplots(figsize=(10, 5))
-    
     # Definir colores únicos para cada equipo
-    colors = ['blue', 'green', 'gold', 'purple', 'orange']
+    colors = ['blue', 'green', 'red', 'purple', 'orange']
     
     # Crear el gráfico de dispersión con triángulos para cada equipo
+    fig = go.Figure()
+    
     for idx, row in data.iterrows():
-        ax.scatter(row['Name team'], row['Score modelo'], color=colors[idx], marker='^', s=100)
-        ax.hlines(row['Score modelo'] + row['STD'], xmin=-0.5, xmax=4.5, color=colors[idx], linestyle='--')
-    
+        fig.add_trace(go.Scatter(x=[row['Name team']], 
+                                 y=[row['Score modelo']],
+                                 mode='markers',
+                                 marker=dict(symbol='triangle-up', 
+                                             size=15, 
+                                             color=colors[idx]),
+                                 name=row['Name team']))
+        
+        fig.add_shape(go.layout.Shape(
+            type="line",
+            x0=row['Name team'],
+            y0=row['Score modelo'],
+            x1=row['Name team'],
+            y1=row['Score modelo'] + row['STD'],
+            line=dict(color=colors[idx], dash="dash")
+        ))
+        
     # Agregar la línea punteada roja para el threshold
-    ax.hlines(0.90, xmin=-0.5, xmax=4.5, color='red', linestyle='-')
-    ax.set_ylim(0.7, 1.0)
-    ax.set_title(title)
+    fig.add_shape(go.layout.Shape(
+            type="line",
+            x0=data['Name team'].iloc[0],
+            y0=0.90,
+            x1=data['Name team'].iloc[-1],
+            y1=0.90,
+            line=dict(color='red', dash="dash")
+        ))
     
-    # Agregar cuadrícula
-    ax.grid(True)
+    fig.update_layout(title=title, 
+                      yaxis_range=[0.7, 1.0], 
+                      xaxis_title='Team',
+                      yaxis_title='Score',
+                      showlegend=True,
+                      gridcolor="gray")
     
-    st.pyplot(fig)
+    st.plotly_chart(fig)
 
+st.write(data[['Name team', 'Score modelo', 'STD']])
 
 # Gráfica Neural Network Model
-st.write('Neural Network Model Table')
-st.write(data[['Name team', 'Score modelo', 'STD']])
 st.subheader('Neural Network Model')
 plot_data(data, 'Neural Network Model Scores')
+st.write('Neural Network Model Table')
 
 # Gráfica Time Series Model 
 st.subheader('Time Series Model')
 plot_data(data, 'Time Series Model Scores')
 st.write('Time Series Model Table')
-st.write(data[['Name team', 'Score modelo', 'STD']])
+st.write(data[['Name team', 'Score modelo']])
