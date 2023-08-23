6c3a8179d7cde97e74b810bf1d580e8d23bcfb9a
Fernando-Aguilar1 <fernando.aguilar@baubap.com>
2023-08-23 17:32:34 -0600

    Change code

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import streamlit as st
-import plotly.graph_objects as go
 
 # Función para cargar los datos
 def load_data(file_name: str):
@@ -13,58 +12,18 @@ st.title('Ranking Team')
 
 # Función para graficar los datos
 def plot_data(data, title):
-    # Definir colores únicos para cada equipo
-    colors = ['blue', 'green', 'red', 'purple', 'orange']
+    chart = st.bar_chart(data.set_index("Name team")["Score modelo"])
+    # Aquí tendrías que agregar la lógica para mostrar las líneas de STD, pero la API nativa de Streamlit no proporciona una forma fácil de hacerlo.
+    # Si el STD es absolutamente necesario, es posible que necesites otra biblioteca (como bokeh o plotly) y que luego la integres con st.bokeh_chart o st.plotly_chart respectivamente.
     
-    # Crear el gráfico de dispersión con triángulos para cada equipo
-    fig = go.Figure()
-    
-    for idx, row in data.iterrows():
-        fig.add_trace(go.Scatter(x=[row['Name team']], 
-                                 y=[row['Score modelo']],
-                                 mode='markers',
-                                 marker=dict(symbol='triangle-up', 
-                                             size=15, 
-                                             color=colors[idx]),
-                                 name=row['Name team']))
-        
-        fig.add_shape(go.layout.Shape(
-            type="line",
-            x0=row['Name team'],
-            y0=row['Score modelo'],
-            x1=row['Name team'],
-            y1=row['Score modelo'] + row['STD'],
-            line=dict(color=colors[idx], dash="dash")
-        ))
-        
-    # Agregar la línea punteada roja para el threshold
-    fig.add_shape(go.layout.Shape(
-            type="line",
-            x0=data['Name team'].iloc[0],
-            y0=0.90,
-            x1=data['Name team'].iloc[-1],
-            y1=0.90,
-            line=dict(color='red', dash="dash")
-        ))
-    
-    fig.update_layout(title=title, 
-                      yaxis_range=[0.7, 1.0], 
-                      xaxis_title='Team',
-                      yaxis_title='Score',
-                      showlegend=True,
-                      gridcolor="gray")
-    
-    st.plotly_chart(fig)
-
-st.write(data[['Name team', 'Score modelo', 'STD']])
-
 # Gráfica Neural Network Model
+st.write('Neural Network Model Table')
+st.write(data[['Name team', 'Score modelo', 'STD']])
 st.subheader('Neural Network Model')
 plot_data(data, 'Neural Network Model Scores')
-st.write('Neural Network Model Table')
 
 # Gráfica Time Series Model 
 st.subheader('Time Series Model')
 plot_data(data, 'Time Series Model Scores')
 st.write('Time Series Model Table')
-st.write(data[['Name team', 'Score modelo']])
+st.write(data[['Name team', 'Score modelo', 'STD']])
