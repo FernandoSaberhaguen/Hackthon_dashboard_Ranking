b64412ea24bc846ec78098177fda18f7bfddf7c3
Fernando-Aguilar1 <fernando.aguilar@baubap.com>
2023-08-23 16:53:20 -0600

    Change code_v2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
 
 # Función para cargar los datos
 def load_data(file_name: str):
@@ -12,27 +13,32 @@ st.title('Ranking Team')
 
 # Función para graficar los datos
 def plot_data(data, title):
-    st.subheader(title)
-
-    # Gráfica del Score modelo
-    chart_data = data.set_index('Name team')[['Score modelo']]
-    st.line_chart(chart_data, use_container_width=True)
-
-    # Gráfica de la STD añadida al Score modelo
-    data['Score + STD'] = data['Score modelo'] + data['STD']
-    st.line_chart(data.set_index('Name team')[['Score + STD']], use_container_width=True, color="orange")
-
-    # Linea del threshold
-    threshold = pd.DataFrame([0.90] * len(data), columns=['Threshold'], index=data['Name team'])
-    st.line_chart(threshold, use_container_width=True, color="red")
+    fig, ax = plt.subplots(figsize=(10, 5))
     
+    # Definir colores únicos para cada equipo
+    colors = ['blue', 'green', 'red', 'purple', 'orange']
+    
+    # Crear el gráfico de dispersión con triángulos para cada equipo
+    for idx, row in data.iterrows():
+        ax.scatter(row['Name team'], row['Score modelo'], color=colors[idx], marker='^', s=100)
+        ax.hlines(row['Score modelo'] + row['STD'], xmin=-0.5, xmax=4.5, color=colors[idx], linestyle='--')
+    
+    # Agregar la línea punteada roja para el threshold
+    ax.hlines(0.90, xmin=-0.5, xmax=4.5, color='red', linestyle='--')
+    ax.set_ylim(0.7, 1.0)
+    ax.set_title(title)
+    
+    st.pyplot(fig)
+
 st.write(data[['Name team', 'Score modelo', 'STD']])
 
 # Gráfica Neural Network Model
+st.subheader('Neural Network Model')
 plot_data(data, 'Neural Network Model Scores')
 st.write('Neural Network Model Table')
 
 # Gráfica Time Series Model 
+st.subheader('Time Series Model')
 plot_data(data, 'Time Series Model Scores')
 st.write('Time Series Model Table')
-st.write(data[['Name team', 'Score modelo', 'STD']])
+st.write(data[['Name team', 'Score modelo']])
