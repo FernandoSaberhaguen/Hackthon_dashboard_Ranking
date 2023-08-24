23ea396565884fe1590f233091c36c446a34f040
Fernando-Aguilar1 <fernando.aguilar@baubap.com>
2023-08-24 13:51:45 -0600

    change data viz

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
 
 # Función para cargar los datos
 def load_data(file_name: str):
@@ -12,18 +13,38 @@ st.title('Ranking Team')
 
 # Función para graficar los datos
 def plot_data(data, title):
-    chart = st.bar_chart(data.set_index("Name team")["Score modelo"])
-    # Aquí tendrías que agregar la lógica para mostrar las líneas de STD, pero la API nativa de Streamlit no proporciona una forma fácil de hacerlo.
-    # Si el STD es absolutamente necesario, es posible que necesites otra biblioteca (como bokeh o plotly) y que luego la integres con st.bokeh_chart o st.plotly_chart respectivamente.
+    fig, ax = plt.subplots(figsize=(10, 5))
     
-# Gráfica Neural Network Model
-st.write('Neural Network Model Table')
+    # Definir colores únicos para cada equipo
+    colors = ['blue', 'green', 'red', 'purple', 'orange']
+    
+    teams = data['Name team'].tolist()
+    
+    # Para cada equipo, plotear el score y el STD
+    for idx, row in data.iterrows():
+        ax.plot([row['Name team'], row['Name team']], [row['Score modelo'], row['Score modelo']], color=colors[idx], linestyle='--')
+        ax.scatter(row['Name team'], row['Score modelo'] + row['STD'], color=colors[idx], marker='^', s=100)
+    
+    # Establecer las etiquetas del eje x como nombres de equipo
+    ax.set_xticks(teams)
+    
+    # Agregar la línea punteada roja para el threshold
+    ax.hlines(0.90, xmin=teams[0], xmax=teams[-1], color='red', linestyle='--')
+    ax.set_ylim(0.7, 1.0)
+    ax.set_title(title)
+    
+    st.pyplot(fig)
+
 st.write(data[['Name team', 'Score modelo', 'STD']])
+
+# Gráfica Neural Network Model
 st.subheader('Neural Network Model')
 plot_data(data, 'Neural Network Model Scores')
+st.write('Neural Network Model Table')
 
 # Gráfica Time Series Model 
 st.subheader('Time Series Model')
 plot_data(data, 'Time Series Model Scores')
 st.write('Time Series Model Table')
 st.write(data[['Name team', 'Score modelo', 'STD']])
+
